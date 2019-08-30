29f2582ea26f62c159afed119ab93ddefd014d7d
plonsdale1 <petex.1995@outlook.com>
2019-08-30 02:12:55 +0000

    refactered with promises!

---
+++ iss_promised.js
@@ -0,0 +1,15 @@
+
+
+const request = require('request-promise-native');
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
